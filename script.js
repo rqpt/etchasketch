663c173998d4aaf27aa7500c81fc47c48635c2b4
rqpt <1ernstvermeulen1@gmail.com>
2022-09-20 15:44:04 +0200

    Organise grid in final 16x16 config

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-// Create 16x16 divs and place them inside the container div
-
-// Declare a create div element variable
-// Select the container class div in the html
-// append the div 256 times to the container
-
 const divCont = document.querySelector('.container');
 
-for (i = 0; i <= 256; i++) {
+for (i = 0; i < 256; i++) {
 	const gridDiv = document.createElement('div');
 	divCont.appendChild(gridDiv);
 };
