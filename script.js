8f33b5c564f2ee0d9fd829cd72bf9e2a9dcfb039
rqpt <1ernstvermeulen1@gmail.com>
2022-09-21 08:49:33 +0200

    Working user specified equal sided grid layout

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
 const divCont = document.querySelector('.container');
 
-// Create a button that, when clicked, prompts the user for a number lower than 100
-// Use the user input to generate a grid of divs with the given number as the size for the grid
-
-// Select the button element on the page
-// add a click event listener to the button
-// click prompts the user for a number less than 100
-// if the user inputs not a number or a number greater than 100
-// alert try again
-// else set i variable to 0
-// while i is lesser than user input
-// append tiles to the div container
-//
 const sizeButton = document.querySelector('button');
 sizeButton.addEventListener('click', getUserInput);
 
 function getUserInput() {
 	const userInput = +prompt('Input a number between 1 and 100');
-	if (typeof userInput !== 'number') {
-		alert('try again');
+	if ((typeof userInput !== 'number') || (userInput >= 100)){
+		alert('Try Again');
 	} else {
-		for (i = 0; i < userInput**2; i++) {
-			const tile = document.createElement('div');
-			tile.classList.add('tile');
-			divCont.appendChild(tile);
+		for (i = 0; i < userInput; i++) {
+			const column = document.createElement('div');
+			column.classList.add('column');
+			divCont.appendChild(column);
 		}
-		activateGrid();
+		const columns = document.querySelectorAll('.column');
+		columns.forEach((column) => {
+			for (i = 0; i < userInput; i++) {
+				const row = document.createElement('div');
+				row.classList.add('row');
+				column.appendChild(row);
+			}
+		});
+	activateGrid();
 	}
 };
 
 function activateGrid() {
-	const grid = document.querySelectorAll('.tile');
+	const grid = document.querySelectorAll('.row', '.column');
 	grid.forEach((tile) => {
 		tile.addEventListener('mouseover', e => {
 			e.target.classList.add('blue');
